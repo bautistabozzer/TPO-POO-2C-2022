2922941ffd5e871a432cf9ce710e0dec65c99bfd
Bautista Bozzer <bozzerbati@gmail.com>
2022-11-16 21:12:06 -0300

    Version 1.7

--- src/uade/edu/ar/dao/PeticionDao.java
+++ src/uade/edu/ar/dao/PeticionDao.java
@@ -7,5 +7,6 @@ import uade.edu.ar.util.GenericDAO;
 public class PeticionDao extends GenericDAO<Peticion> {
     public PeticionDao(Class<Peticion> clase, String file) throws Exception {
         super(clase, file);
+
     }
 }
